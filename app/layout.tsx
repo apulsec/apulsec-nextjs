8625ff3c9bf0e380c6edbf0b1648204f4bdd94e4
apulsec <apulsec@gmail.com>
2026-09-10 20:14:31 +0800

    dev

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Geist_Mono, Inter } from "next/font/google"
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { SiteFloatingDock } from "@/components/site-floating-dock"
-import { NoiseTexture } from "@/components/ui/noise-texture"
 import { cn } from "@/lib/utils";
 
 const inter = Inter({subsets:['latin'],variable:'--font-sans'})
@@ -26,15 +25,7 @@ export default function RootLayout({
     >
       <body>
         <ThemeProvider>
-          <NoiseTexture
-            aria-hidden="true"
-            className="fixed inset-0 z-0 opacity-[0.5] dark:opacity-[0.5]"
-            frequency={0.8}
-            octaves={4}
-            slope={0.3}
-            noiseOpacity={1}
-          />
-          <div className="relative z-10 pb-0">{children}</div>
+          <div className="pb-0">{children}</div>
           <SiteFloatingDock />
         </ThemeProvider>
       </body>
